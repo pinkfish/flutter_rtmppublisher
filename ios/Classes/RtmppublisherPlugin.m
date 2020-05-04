5e356a34eae257cec5693bdb753053a1fde1d36e
David Bennett <pinkfishfrog@gmail.com>
2020-05-04 11:51:50 -0700

    Fix ios build

--- ios/Classes/RtmppublisherPlugin.m
+++ ios/Classes/RtmppublisherPlugin.m
@@ -1,11 +1,11 @@
 #import "RtmppublisherPlugin.h"
-#if __has_include("rtmppublisher/rtmppublisher-Swift.h")
-#import <rtmppublisher/rtmppublisher-Swift.h>
+#if __has_include("camera_with_rtmp/camera_with_rtmp-Swift.h")
+#import <camera_with_rtmp/camera_with_rtmp-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "rtmppublisher-Swift.h"
+#import "camera_with_rtmp-Swift.h"
 #endif
 
 #import <AVFoundation/AVFoundation.h>
