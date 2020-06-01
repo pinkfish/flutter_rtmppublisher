1ff4449be6818803b9dd5b7a34b2b871744063ff
pinkfish <pinkfish@users.noreply.github.com>
2020-05-31 21:04:12 -0700

    Fix syntax

--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "::set-env name=FLUTTER_VERSION=1.17.1"
-echo "::set-env name=JAVA_VERSION=12.x"
+echo "::set-env name=FLUTTER_VERSION::1.17.1"
+echo "::set-env name=JAVA_VERSION::12.x"
